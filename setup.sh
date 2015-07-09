c882442fc33410a3ac2c0f7595b69e885e8a6a7b
David Otterdahl <david.otterdahl@gmail.com>
2015-07-09 10:04:49 +0200

    setup.sh: Spotify not supported for 32-bit anymore

--- setup.sh
+++ setup.sh
@@ -302,8 +302,6 @@ END
     wget http://security.ubuntu.com/ubuntu/pool/main/libg/libgcrypt11/libgcrypt11_1.5.4-2ubuntu1.1_amd64.deb
     sudo dpkg -i libgcrypt11_1.5.4-2ubuntu1.1_amd64.deb
     rm libgcrypt11_1.5.4-2ubuntu1.1_amd64.deb
-
-    #TODO: install spotify-notify for x86 (not needed for x86_64)
 }
 
 function uninstall-spotify () {
