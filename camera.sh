f2bb1ec4c84bcf77be644e6ec7da705333dbd65e
David Otterdahl <david@otterdahl.org>
2015-07-11 17:44:49 +0200

    camera.sh: Move MP4-files. Don't mount/umount

--- camera.sh
+++ camera.sh
@@ -2,12 +2,12 @@
 # camera.sh
 # Move all photos from memory card to a given folder
 # Sort photos into sub folders based on date in EXIF data
+# Requires ImageMagick
 set -e
 
-ORIGIN=/mnt/mmcblk0p1
+ORIGIN=`mount | grep mmcblk0p1 | awk '{print $3}'`
 DEST=~/Bilder
 
-mount | grep $ORIGIN || mount $ORIGIN
 
 # Moves file names ending with JPG
 find $ORIGIN -iregex ".*JPG" -prune -print0 | while read -d $'\0' file
@@ -20,4 +20,12 @@ do
     mv -i "$file" "$DEST/$DATE"
 done
 
-umount $ORIGIN
+# Moves file names ending with MP4
+find $ORIGIN -iregex ".*MP4" -prune -print0 | while read -d $'\0' file
+do
+    echo "Moving $file..."
+    if [ ! -d "$DEST" ]; then
+        mkdir -p "$DEST"
+    fi
+    mv -i "$file" "$DEST"
+done
