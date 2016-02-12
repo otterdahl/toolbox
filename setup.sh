ba1e82d2777adb44fbdff09b35b5f9571acb82c3
David Otterdahl <david.otterdahl@gmail.com>
2016-02-12 07:19:18 +0100

    setup.sh: ncmpcpp configuration file name changes

--- setup.sh
+++ setup.sh
@@ -568,7 +568,7 @@ function install-mpd () {
     mkdir -p ~/.ncmpcpp
     touch ~/.config/mpd/pid
     touch ~/.config/mpd/tag_cache
-    ln -fs ~/config/ncmpcpp_keys ~/.ncmpcpp/keys
+    ln -fs ~/config/ncmpcpp_keys ~/.ncmpcpp/bindings
 
     # On Raspbian. Uses ~/.mpdconf
     # On Ubuntu 14.04. Uses ~/.config/mpd/mpd.conf
