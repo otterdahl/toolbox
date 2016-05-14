785ce7821d21a2db6fab5318d0e21f60908eee34
David Otterdahl <david.otterdahl@gmail.com>
2016-05-14 19:55:53 +0200

    setup.sh: Add experiences from Arch Linux

--- setup.sh
+++ setup.sh
@@ -7,17 +7,42 @@ set -e
 
 # Install essential applications
 function install-essential () {
+    # Ubuntu
     sudo apt-get install task vim lynx cifs-utils git screen catdoc powertop \
          wvdial bridge-utils pdftk dvb-apps w-scan libav-tools at imagemagick \
          curl opus-tools irssi bitlbee-libpurple
-    
+
+    # Arch Linux
+    # sudo pacman -S git vim cron syncthing task screen ghostdriver imagemagick \
+    # lynx wget unzip networkmanager
+    #
+    # systemctl enable NetworkManager
+
     # Desktop
     sudo apt-get install virt-manager i3 feh rdesktop mpv mplayer2 vlc thunar \
         gnome-icon-theme-full scrot xscreensaver autocutsel rxvt-unicode-256color \
         libjson-perl pavucontrol
 
+    # Arch Linux
+    # sudo pacman -S lightdm i3-wm i3status dmenu rxvt-unicode mpv feh vlc firefox \
+    #    perl-json pavucontrol pulseaudio thunar network-manager-applet zathura-pdf-mupdf \
+    #    ttf-inconsolata xorg-xrdb xorg-modmap
+    #
+    # AUR makepkg -sri
+    # pdftk (testing)
+    # kpcli
+    # spotify
+    # steam
+    # xf86-input-mtrack
+    # dropbox
+    # mbpfan-git
+    # sudo systemctl enable mbpfan.service
+
     # Email
     sudo apt-get install mutt procmail offlineimap msmtp
+
+    # Arch Linux
+    # sudo pacman -S mutt procmail
 
     # Maildirproc
     sudo apt-get install python3-3to2
@@ -34,6 +59,24 @@ function install-essential () {
         sudo apt-get -y install svtplay-dl
     fi
 }
+
+# Install macbookpro 8,2 Arch Linux
+# 1, Follow beginners guide https://wiki.archlinux.org/index.php/beginners'_guide
+# 2, Use UEFI/GPT bootloader, systemd-boot.
+# 3, Use kernel options to turn off integrated graphics. Hold space and then
+#    press `e` during boot and add `radeon.modeset=0 i915.modeset=1 i915.lvds_channel_mode=2`
+#    To make permanent, example config in /boot/loader/entries:
+#    title   Arch Linux
+#    linux   /vmlinuz-linux
+#    initrd  /intel-ucode.img
+#    initrd  /initramfs-linux.img
+#    options root=PARTUUID=03b57a03-85a9-4d29-879c-5973cb0186be rw radeon.modeset=0 i915.modeset=1 i915.lvds_channel_mode=2
+# 4, In order to start xorg you need to switch gpu
+#    Use https://github.com/0xbb/gpu-switch
+#    gpu-switch -i
+# 5, keyboard in x11:
+#    setxkbmap -model pc104 -layout se
+#    ~/.xinitrc
 
 function install-macbook () {
     # fan control daemon for Apple MacBook / MacBook Pro computers
@@ -156,6 +199,9 @@ END
     # Set irssi config
     ln -f -s ~/config/irssi ~/.irssi
 
+    # Set mailcap
+    ln -f -s ~/config/mailcap ~/.mailcap
+
     # Configure crontab
     crontab ~/config/crontab
 
@@ -192,6 +238,10 @@ function uninstall-pipelight () {
 
 # Wifi drivers for Edimax AC-1200 (7392:a822) and Zyxel NWD6505
 function install-edimax () {
+
+    # ARCH
+    # pacman -S linux-headers
+
     cd $INSTALLDIR
     if [ ! -d rtl8812AU_8821AU_linux ]; then
         git clone https://github.com/abperiasamy/rtl8812AU_8821AU_linux.git
@@ -218,6 +268,18 @@ function uninstall-edimax () {
 function install-canon-p150 () {
     cd $INSTALLDIR
 
+    # Arch Linux
+    # pacman -S sane simple-scan
+    #
+    # Enable multilib. Driver is partly 32-bit
+    # pacman -S lib32-glibc lib32-gcc-libs
+    #
+    # Write udev rule:
+    # Add
+    # # Canon P-150
+    # ATTRS.... 1083, 162c
+    # to /lib/udev/rules.d/49-sane.rules
+
     # Download driver
     mkdir -p canon
     cd canon
@@ -234,6 +296,13 @@ function install-canon-p150 () {
     rm 150_LINUX_V10.zip
 
     # taken from: http://lowerstrata.blogspot.se/2010/07/canon-p-150-and-linux.html
+
+	# Ubuntu Linux
+    sudo apt-get install libusb-dev
+
+    # Arch Linux
+	# pacman -S libusb-compat
+
     sudo apt-get install libusb-dev
     tar xfz cndrvsane-p150-1.00-0.2.tar.gz
     wget -q https://alioth.debian.org/frs/download.php/file/2318/sane-backends-1.0.19.tar.gz
@@ -242,10 +311,29 @@ function install-canon-p150 () {
     ./configure
     make
     cd ../cndrvsane-p150-1.00-0.2
+
+    # Ubuntu Linux
     fakeroot make -f debian/rules binary
     cd ..
     sudo dpkg -i cndrvsane-p150_1.00-0.2_amd64.deb
     sudo ln -s /opt/Canon/lib/canondr /usr/local/lib/canondr
+
+	# Arch Linux
+	# autoreconf -i
+    # ./configure --prefix=/opt/Canon --docdir=/usr/share
+	# make
+	# sudo make install
+    # cd ..
+	# sudo ln -sf /opt/Canon/etc/sane.d/canondr.conf /etc/sane.d
+	# if [ -n "`grep '#[[:space:]]*canondr' /etc/sane.d/dll.conf`" ];then
+	#	sudo sed -i 's,#[[:space:]]*\canondr\),\1,' /etc/sane.d/dll.conf
+	# elif [ -z "`grep canondr /etc/sane.d/dll.conf`" ]; then
+	#	echo canondr | sudo tee -a /etc/sane.d/dll.conf
+	# fi
+	# sudo ln -sf /opt/Canon/lib/sane/libsane-canondr.so.1.0.0 /usr/lib/sane
+	# sudo ln -sf /opt/Canon/lib/sane/libsane-canondr.so.1.0.0 /usr/lib/sane/libsane-canondr.so.1
+	# sudo ln -sf /opt/Canon/lib/sane/libsane-canondr.so.1.0.0 /usr/lib/sane/libsane-canondr.so
+
     cd ..
     rm -rf $INSTALLDIR/canon
 }
@@ -285,6 +373,7 @@ END
 }
 
 # Citrix Receiver 13.3.0
+# Arch Linux: Exists in AUR. Needs EULA fix + keyboard mapping
 function install-citrix () {
     cd $INSTALLDIR
     sudo dpkg --add-architecture i386 # only needed once
