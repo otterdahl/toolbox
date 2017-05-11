9378a884822203e27e93cb1c10e0252d9b5d65ba
David Otterdahl <david.otterdahl@gmail.com>
2017-05-11 09:55:07 +0200

    Remove tv, cecserver.sh and autorecord.sh

--- audiorecord.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Usage: audiorecord [output directory]
-
-# Record audio using default mic for 9 hours
-MINUTES=540
-FILENAME="`date +'%F_%H:%M'`.ogg"
-timeout "$MINUTES"m arecord -f cd -q -t raw | oggenc - -r -Q -o $FILENAME
-
-mv $FILENAME "$HOME/$1"
