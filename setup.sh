5581e6f2e29772f8da86083af6fd5b4136f12ea2
David Otterdahl <david.otterdahl@gmail.com>
2019-10-07 18:35:32 +0200

    setup.sh: Remove init.d mpd installation

--- setup.sh
+++ setup.sh
@@ -381,16 +381,6 @@ function install-mpd () {
     sudo systemctl disable mpd.service
     sudo systemctl stop mpd.socket
     sudo systemctl disable mpd.socket
-    
-    # old style
-    sudo update-rc.d -f mpd remove
-    if ! grep -q START_MPD /etc/default/mpd; then
-        echo START_MPD=false | sudo tee -a /etc/default/mpd
-    fi
-    sudo sed -i "s/START_MPD=true/START_MPD=false/" /etc/default/mpd
-
-    echo "-----------------------------------------------------------"
-    echo "Add music to $HOME/Musik. Then start listening using ncmpcpp"
 }
 
 function uninstall-mpd () {
