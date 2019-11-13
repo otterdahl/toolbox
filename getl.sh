9382609bd366490c8043c78ec8b4c3018227989c
David Otterdahl <david.otterdahl@gmail.com>
2019-11-13 10:47:05 +0100

    Replace getl.pl with getl.sh
    Replace perl with bash

---
+++ getl.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+USAGE="usage: `basename $0` [-t|--type <type>] <URL>]"
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    echo $USAGE
+    exit 1
+fi
+
+TEMP=`getopt -o t: --long type: -n 'getl.sh' -- "$@"`
+eval set -- "$TEMP"
+
+while true; do
+    case "$1" in
+	-t|--type)
+            case "$2" in
+                "")
+                    shift 2
+                    ;;
+                *)
+                    TYPE="$2"
+                    shift 2
+                    ;;
+            esac
+            ;;
+        --) shift ; break ;;
+    esac
+done
+
+if [ -z "$1" ]; then
+	echo "No URL given"
+	exit 1
+fi
+
+if [ -z $TYPE ]; then
+	wget -nH -nv -r -np $1
+else
+	wget -nH -nv -r -np -A.$TYPE $1
+fi
+
